ad22fbf9ae6cb8fe45bede1f1f369d6c39552cc2
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-11-05 13:30:07 -0800

    Added routes.rb and pseudocode-routes.md

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'movies/show'
-  get 'movies/index'
-  get 'movies/create'
-  get 'movies/update'
-  get 'rentals/index'
-  get 'rentals/show'
-  get 'rentals/update'
-  get 'customer/index'
-  get 'customer/show'
-  get 'customer/create'
-  get 'customer/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :customers, only: [:index]
+  resources :movies, only: [:index, :show, :create]
+
+  post '/rentals/check_out', to: 'rentals#check_out', as: 'check_out'
+  post '/rentals/check_in', to: 'rentals#check_in', as: 'check_in'
+
+  get '/zomg', to: 'rentals#zomg', as :'zomg'
+
 end
