6778ed4a5467fb8a9ad584888d7513fa5267e866
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-11-05 12:41:50 -0800

    rails api setup, customer, rental, movie models created

---
+++ db/migrate/20181105203219_create_customers.rb
@@ -0,0 +1,15 @@
+class CreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.datetime :registered_at
+      t.stringcity :address
+      t.string :state
+      t.string :postal_code
+      t.string :phone
+      t.integer :movies_out_count
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20181105203750_create_rentals.rb
@@ -0,0 +1,11 @@
+class CreateRentals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rentals do |t|
+      t.date :checkout_date
+      t.date :checkin_date
+      t.date :due_date
+
+      t.timestamps
+    end
+  end
+end
