388ba8c160576e8e6e6c3f6de9ec7706586d65c5
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-11-05 15:55:37 -0800

    Fixtures updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_215959) do
+ActiveRecord::Schema.define(version: 2018_11_05_231519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2018_11_05_215959) do
     t.string "state"
     t.string "postal_code"
     t.string "phone"
-    t.integer "movies_out_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "city"
