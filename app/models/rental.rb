bccaf5360cae19dfca2811998df54d85110b3e3e
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-11-06 23:06:11 -0800

    checkout and checkin actions tests built and model and controller pass

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -12,6 +12,31 @@ class Rental < ApplicationRecord
     return !self.checkin_date.nil?
   end
 
+  def checkout_data
+    movie_id = self[:movie_id]
+    rented = Movie.find_by(id: movie_id)
+    if rented.inventory_available > 0
+      rented.inventory_available -= 1
+      rented.save
+      self.checkout_date = Date.today
+      self.due_date = Date.today + 7
+      self.checkin_date = nil
+      # self.save
+      return self
+    else
+      return nil
+    end
+  end
+
+  def checkin_data
+    movie_id = self[:movie_id]
+    rented = Movie.find_by(id: movie_id)
+    rented.inventory_available += 1
+    rented.save
+    self.checkin_date = Date.today
+  end
 end
+
+
 
 # Rental.new(checkout_date: Date.yesterday, due_date: Date.tomorrow, movie_id: 4, customer_id: 1)
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,37 +1,53 @@
 require "test_helper"
+require 'date'
 
 describe RentalsController do
- describe "api actions" do
-   let(:new_rental) {
-     { customer_id: 859843310,
-       movie_id: 400453147
-     }
+  let(:rental) {
+   { customer_id: 859843310,
+     movie_id: 400453147
    }
-   it 'can checkout a movie' do
-     # POST /rentals/check_out
-     # binding.pry
-     fields = %w(id movie_id customer_id due_date checkout_date checkin_date).sort
-     movie_inv_avail_start = Movie.find(400453147).inventory_available
-     expect do
-       post checkout_path, params: new_rental, as: :json
-     end.must_change 'Rental.count', 1
-     movie_inv_avail_end = Movie.find(400453147).inventory_available
-     value(response).must_be :successful?
-     expect(response.header['Content-Type']).must_include 'json'
-     body = JSON.parse(response.body)
-     body.each do |rental|
-       rental.keys.sort.must_equal fields
-     end
+  }
 
-     expect(body.checkout_date).must_be Date.today
-     expect(body.checkin_date).must_be_nil
-     expect(body.due_date).must_be Date.today + 7
-     expect(movie_inv_avail_end).must_equal movie_inv_avail_start - 1
-   end
+  it 'can checkout a movie' do
+   # POST /rentals/check_out
+   fields = %w(id movie_id customer_id due_date checkout_date checkin_date).sort
+   movie_inv_avail_start = Movie.find(400453147).inventory_available
+   expect do
+     post check_out_path, params: rental, as: :json
+   end.must_change 'Rental.count', 1
 
-   it 'can checkin a movie' do
-     # POST /rentals/check_inout
-     # post '/rentals/check_in'
-   end
- end
+   movie_inv_avail_end = Movie.find(400453147).inventory_available
+   value(response).must_be :successful?
+
+   expect(response.header['Content-Type']).must_include 'json'
+   body = JSON.parse(response.body)
+
+   expect(body.keys.sort).must_equal fields
+   expect(body["checkout_date"]).must_equal Date.today.to_s
+   expect(body["checkin_date"]).must_be_nil
+   expect(body["due_date"]).must_equal (Date.today + 7).to_s
+   expect(movie_inv_avail_end).must_equal movie_inv_avail_start - 1
+  end
+
+  it 'can checkin a movie' do
+   # post '/rentals/check_in'
+   fields = %w(id movie_id customer_id due_date checkout_date checkin_date).sort
+   post check_out_path, params: rental
+   movie_inv_avail_start = Movie.find(400453147).inventory_available
+
+   expect do
+     post check_in_path, params: rental, as: :json
+   end.wont_change 'Rental.count'
+
+   movie_inv_avail_end = Movie.find(400453147).inventory_available
+   value(response).must_be :successful?
+
+   expect(response.header['Content-Type']).must_include 'json'
+   body = JSON.parse(response.body)
+
+   expect(body.keys.sort).must_equal fields
+   expect(body["checkin_date"]).must_equal Date.today.to_s
+   expect(movie_inv_avail_end).must_equal movie_inv_avail_start + 1
+  end
 end
+
