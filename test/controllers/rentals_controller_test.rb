7632b3bbf81987ae43d30cf8b796794f46fa4eb5
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-11-07 13:13:21 -0800

    Added 2 tests to mimic postman smoke test

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -49,5 +49,72 @@ describe RentalsController do
    expect(body["checkin_date"]).must_equal Date.today.to_s
    expect(movie_inv_avail_end).must_equal movie_inv_avail_start + 1
   end
-end
 
+  describe 'SJL: mimicking the postman wave3 smoke test' do
+    let (:customer) { customers(:customer_out)}
+    let (:movie) { movies(:movie_in)}
+    let(:new_rental_params) {
+     { customer_id: customer.id,
+       movie_id: movie.id
+     }
+    }
+    it 'checking OUT a movie increases customer mcoc count by one' do
+
+      # ARRANGE: FIND CUSTOMER, CHECK START COUNT
+      # CUSTOMER CHECKS OUT A MOVIE
+      get customers_path, as: :json
+      body = JSON.parse(response.body) # array of all customers
+      body = body.select {|cust| cust["id"] == customer.id} # array of a single customer hash
+      mcoc_start = body[0]["movies_checked_out_count"]
+
+      expect(mcoc_start).must_equal 3 # just to be sure of our fixtures
+
+      post check_out_path, params: new_rental_params, as: :json
+      value(response).must_be :successful?
+
+      # # ACT: FIND SAME CUSTOMER, CHECK END COUNT
+      get customers_path, as: :json
+      body2 = JSON.parse(response.body) # array of all customers
+      body2.select {|cust| cust["id"] == customer.id} # array of a single customer hash
+
+      # # ASSERT: END COUNT = START COUNT + 1
+      mcoc_end = body2[0]["movies_checked_out_count"]
+      expect(mcoc_end).must_equal (mcoc_start + 1)
+      expect(mcoc_end).must_equal 4 # just to be sure again.
+    end
+
+    it 'checking IN a movie decreases customer mcoc count by one' do
+      # ZERO
+      get customers_path, as: :json
+      value(response).must_be :successful?
+      body = JSON.parse(response.body) # array of all customers
+      body = body.select {|cust| cust["id"] == customer.id} # array of a single customer hash
+      mcoc_zero = body[0]["movies_checked_out_count"]
+      expect(mcoc_zero).must_equal 3 # from fixtures
+
+      # CHECK OUT
+      post check_out_path, params: new_rental_params, as: :json
+      value(response).must_be :successful?
+
+      # BEFORE
+      get customers_path, as: :json
+      value(response).must_be :successful?
+      body2 = JSON.parse(response.body) # array of all customers
+      body2.select {|cust| cust["id"] == customer.id}
+      mcoc_before = body2[0]["movies_checked_out_count"]
+      expect(mcoc_before).must_equal 4 # mcoc_zero + 1
+
+      # CHECK IN
+      post check_out_path, params: new_rental_params, as: :json
+      value(response).must_be :successful?
+
+      # AFTER
+      get customers_path, as: :json
+      value(response).must_be :successful?
+      body3 = JSON.parse(response.body) # array of all customers
+      body3.select {|cust| cust["id"] == customer.id}
+      mcoc_end = body3[0]["movies_checked_out_count"]
+      expect(mcoc_end).must_equal 3 #mcoc_zero
+    end
+  end
+end
