8ed29cf3174f43d0bd4dfa6acbd64add3d0dbf74
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-11-06 19:20:57 -0800

    Small refactors to movies contrl and cust ctrl tests

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -39,28 +39,27 @@ describe Rental do
 
 
   describe 'relationships' do
-    let(:rental) { rentals(:rental_in)  }
 
     it 'belongs to a customer' do
       # Arrange (done with let)
 
       # Act
-      customer = rental.customer
+      customer = rental_in.customer
 
       # Assert
       expect(customer).must_be_instance_of Customer
-      expect(customer.id).must_equal rental.customer_id
+      expect(customer.id).must_equal rental_in.customer_id
     end
 
     it 'belongs to a movie' do
       # Arrange (done with let)
 
       # Act
-      movie = rental.movie
+      movie = rental_in.movie
 
       # Assert
       expect(movie).must_be_instance_of Movie
-      expect(movie.id).must_equal rental.movie_id
+      expect(movie.id).must_equal rental_in.movie_id
     end
   end
 end
