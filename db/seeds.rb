be2b82978c45fbe5a6546ac6aba42456fdbc0cec
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-11-09 01:33:13 -0800

    Updated seed data- overdue rentals, upped movie inventory #s

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,17 @@ JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
   Customer.create!(customer)
 end
 
-JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
+JSON.parse(File.read('db/seeds/movies.json')).each_with_index do |movie, index|
   movie["inventory_available"] = movie["inventory"]
-  Movie.create!(movie)
+  unless Movie.create(movie)
+    puts "failed to create Movie #{index}: #{movie["title"]}"
+  end
 end
 
 # Movie.update_all("inventory_available=inventory")
+
+JSON.parse(File.read('db/seeds/rentals.json'), :quirks_mode => true).each do |rental|
+  unless Rental.create(rental)
+    puts "failed to create rental #{index}: #{rental["movie_id"]}"
+  end
+end
